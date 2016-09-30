5dbc650d5fe45afd908e273c5f1f5cb926807332
Krisofer Brink <kpgbrink@gmail.com>
2016-09-30 15:31:26 -0400

    Add files via upload
    Files

---
+++ app/src/main/java/edu/calvin/kpb23students/AboutActivity.java
@@ -0,0 +1,28 @@
+package edu.calvin.kpb23students.lab04;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * This is the about activity.
+ * <p/>
+ * For use as CS 262, Lab 4
+ *
+ * @author Kristofer
+ * @version Fall, 2016
+ */
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
